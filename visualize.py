35c6754154b320c7c59da2d140276563ee163ce9
Mike Gao <tenggao@gmail.com>
2019-06-22 14:21:45 +0800

    add group box plot

--- visualize.py
+++ visualize.py
@@ -31,7 +31,7 @@ def histogram_grid(data, histfunc='count', asFigure=False, **layout):
 
 def scatter_target_plot(data, select_columns, target, num_cols=3, asFigure=False, **layout):
     num_rows = ceil(len(select_columns)/num_cols)
-    fig = tools.make_subplots(rows=num_rows, cols=num_cols, print_grid=False, subplot_titles=numeric_columns)
+    fig = tools.make_subplots(rows=num_rows, cols=num_cols, print_grid=False, subplot_titles=select_columns)
     for i, col in enumerate(select_columns):
         fig.append_trace(
             go.Scatter(x=data[col], y=data[target], mode='markers'),
@@ -45,6 +45,24 @@ def scatter_target_plot(data, select_columns, target, num_cols=3, asFigure=False
         showlegend=False, 
         template='plotly_dark', 
         title=f'Select Columns Value VS {target}')
+    fig['layout'].update(layout)
+    if asFigure:
+        return fig
+    else:
+        iplot(fig)
+
+def group_boxplot(data, asFigure=False, log_transform=False,  **layout):
+    traces = []
+    for col in data.columns:
+        traces.append(
+            go.Box(
+                x = np.log1p(data[col]) if log_transform else x,
+                name = col
+            )
+        )
+    fig = go.Figure(data=traces)
+    fig['layout'].update(title='Distribution', template='plotly_dark')
+    fig['layout'].update(layout)
     if asFigure:
         return fig
     else:
